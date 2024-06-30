94439f777c91ea0070e5e6db805ff1875bb37a8d
kawan2004 <kawanbarros2004@gmail.com>
2024-06-30 20:54:25 -0300

    Implementar metodo para guardar fotos

--- src/capturar_face.py
+++ src/capturar_face.py
@@ -1,4 +1,5 @@
 import cv2 as cv
+import os
 
 class capturar_face:
 
@@ -6,6 +7,7 @@ class capturar_face:
         
         self.video = cv.VideoCapture (0) # objeto que inicializa a camera principal (0)
         self.classificador_face = cv.CascadeClassifier (cv.data.haarcascades + 'haarcascade_frontalface_default.xml') # modelo pre treinado
+        self.contador = 0
 
     def iniciar_camera (self):
         
@@ -16,9 +18,23 @@ class capturar_face:
             if (not ret): # verifica se o frame pode ser capturado
                 break
 
-            gray = cv.cvtColor (self.detectar_face(frame), cv.COLOR_BGR2GRAY) # processamento de imagem, que transforma em escala cinza
+            gray = cv.cvtColor (frame, cv.COLOR_BGR2GRAY) # processamento de imagem, que transforma em escala cinza
 
             cv.imshow ('Janela', gray) # exibe a camera
+
+            if cv.waitKey (1) & 0xFF == ord('c'): # condicional para capturar rosto
+
+                os.makedirs("imagens", exist_ok=True) # criando diretorio para guardar imagens
+
+                nome_img = f"img-{self.contador}.jpg"
+                path = os.path.join("imagens", nome_img)
+
+                cv.imwrite(path, gray) # metodo para guardar imagens
+                
+                print(f"imagem capturada com sucesso!")
+
+                self.contador += 1  # Incrementar o contador para o próximo arquivo
+
 
             if cv.waitKey (1) & 0xFF == ord('q'): # condicional para encerrar a execução
                 break
--- src/main.py
+++ src/main.py
@@ -2,3 +2,17 @@ from capturar_face import capturar_face
 
 camera = capturar_face ()
 camera.iniciar_camera()
+
+'''
+
+print ("\n1 - Cadastrar rosto\n2 - Realizar reconhecimento facial\n3 - Sair")
+
+resposta = int (input ("\n> "))
+
+if (resposta == 1):
+    pass    
+
+elif (resposta == 2):   
+    pass
+    
+'''
