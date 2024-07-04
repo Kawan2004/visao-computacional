213b3ad30a344f8a6537fe43084a9140f79008e3
kawan2004 <kawanbarros2004@gmail.com>
2024-07-03 23:46:34 -0300

    corrigir erros

--- src/main.py
+++ src/main.py
@@ -1,6 +1,10 @@
 from cadastrar_face import CadastrarFace
 from reconhecimento_facial import ReconhecimentoFacial
 
+# mains
+
 a = CadastrarFace ("kawan")
 
 a.capturar_imagens ()
+
+b = ReconhecimentoFacial ()
